8d49ea43a3eefd2bf21e952058d989069fb31dcc
yusuken1121 <yusuken1121@gmail.com>
2024-08-17 22:14:07 -0700

    🔧 Add the axios config file

---
+++ frontend/config/apiClient.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:5050", // API server
+  // baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiClient;
