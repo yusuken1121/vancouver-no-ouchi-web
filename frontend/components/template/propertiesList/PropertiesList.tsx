0fffbaa729a18e10f4a30d372ec1b379b7d12520
yusuken1121 <yusuken1121@gmail.com>
2024-08-24 10:19:47 -0700

    ⚡️ Fix the issue where reloading the website returns the user to the first page

--- frontend/components/template/propertiesList/PropertiesList.tsx
+++ frontend/components/template/propertiesList/PropertiesList.tsx
@@ -2,13 +2,19 @@
 import { PaginationProperty } from "@/components/atoms/propertiesList/PaginationProperty";
 import PropertyCards from "@/components/organisms/propertiesList/PropertyCards";
 import { useFetchPropertyData } from "@/hooks/useFetchPropertyData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const PropertiesList = () => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(() => {
+    const savedPage = Number(localStorage.getItem("currentPage"));
+    return savedPage ? Number(savedPage) : 1;
+  });
   const { properties } = useFetchPropertyData();
   const totalPages = Math.ceil(properties.length / 20);
 
+  useEffect(() => {
+    localStorage.setItem("currentPage", String(currentPage));
+  }, [currentPage]);
   return (
     <div className="flex flex-col gap-8 mb-2">
       <PropertyCards currentPage={currentPage} />
