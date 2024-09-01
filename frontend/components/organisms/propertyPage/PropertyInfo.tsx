faa03179265a15b8922761e9dfa2bd24ba797465
yusuken1121 <yusuken1121@gmail.com>
2024-08-31 19:04:31 -0700

    ⚡️ Fix the width of the Tabcard

--- frontend/components/organisms/propertyPage/PropertyInfo.tsx
+++ frontend/components/organisms/propertyPage/PropertyInfo.tsx
@@ -1,20 +1,16 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { propertyTabsOptions } from "@/utlis/commonOptions";
 
 export function PropertyTabs() {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
+    <Tabs defaultValue={propertyTabsOptions[0].value} className="w-full">
       <TabsList className="grid w-full grid-cols-4">
         {propertyTabsOptions.map((o, index) => (
           <>
@@ -23,65 +19,21 @@ export function PropertyTabs() {
             </TabsTrigger>
           </>
         ))}
-        {propertyTabsOptions.map((o, index) => (
-          <TabsContent key={index} value={o.value}>
-            <Card>
-              <CardHeader>
-                <CardTitle>{o.label}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>Here is the content for {o.label}</p>
-              </CardContent>
-            </Card>
-          </TabsContent>
-        ))}
       </TabsList>
-      <TabsContent value="account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
+      {propertyTabsOptions.map((o, index) => (
+        <TabsContent key={index} value={o.value}>
+          <Card>
+            <CardHeader>
+              <CardTitle>{o.label}</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <CardDescription>
+                Here is the content for {o.label}
+              </CardDescription>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      ))}
     </Tabs>
   );
 }
