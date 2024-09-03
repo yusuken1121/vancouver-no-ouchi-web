0224d0d65d51eed06896bf183c1bc29aa1f62048
yusuken1121 <yusuken1121@gmail.com>
2024-09-02 19:57:13 -0700

    ⚰️ Remove the unnecessary code

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,10 +1,8 @@
-import MyForm from "@/components/molecules/Form";
-import Form from "@/components/molecules/Form";
 import React from "react";
 
 const Home = () => {
   return (
-    <main className="w-full flex items-center justify-center bg-slate-100">
+    <main className="w-full flex items-center justify-center">
       {/* <MyForm /> */}
     </main>
   );
