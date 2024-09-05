c975c40e382810a930f092ef77081eb0765ec9c3
yusuken1121 <yusuken1121@gmail.com>
2024-09-04 23:31:55 -0700

    ✨ Add the logo

--- frontend/components/organisms/Header.tsx
+++ frontend/components/organisms/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import HomeNav from "../atoms/header/HomeNav";
 import { HamburgerMenu } from "../molecules/header/HamburgerMenu";
 import NavMenu from "../molecules/header/NavMenu";
@@ -6,7 +7,13 @@ const Header = () => {
   return (
     <header className="flex h-14 justify-center shadow-md bg-richBlue text-white">
       <div className="base-px relative z-50 w-screen flex items-center justify-between">
-        <div>Logo</div>
+        <Image
+          src="/VancouverNoOuchi.png"
+          width={80}
+          height={70}
+          style={{ objectFit: "cover" }}
+          alt="VancouverNoOuchi Image"
+        />
         <div className="hidden md:block">
           <NavMenu />
         </div>
