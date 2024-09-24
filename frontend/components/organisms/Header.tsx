87f1513412827846de2081dd0d97ab52428f953b
yusuken1121 <yusuken1121@gmail.com>
2024-09-24 13:13:15 -0700

    ⚡️ Add link to the header when a user click the logo

--- frontend/components/organisms/Header.tsx
+++ frontend/components/organisms/Header.tsx
@@ -2,19 +2,23 @@ import Image from "next/image";
 import HomeNav from "../atoms/header/HomeNav";
 import { HamburgerMenu } from "../molecules/header/HamburgerMenu";
 import NavMenu from "../molecules/header/NavMenu";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex h-14 justify-center shadow-md bg-themeColor text-white">
       <div className="base-px relative z-50 w-screen flex items-center justify-between">
-        <Image
-          src="https://i.imgur.com/XIU12AP.png"
-          width={80}
-          height={70}
-          style={{ objectFit: "cover" }}
-          alt="VancouverNoOuchi Image"
-          unoptimized={true}
-        />
+        <Link href={"/properties"}>
+          <Image
+            src="https://i.imgur.com/XIU12AP.png"
+            width={80}
+            height={70}
+            style={{ objectFit: "cover" }}
+            alt="VancouverNoOuchi Image"
+            unoptimized={true}
+          />
+        </Link>
+
         <div className="hidden md:block">
           <NavMenu />
         </div>
