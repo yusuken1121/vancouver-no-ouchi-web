d1009a7408060fa1cc29ab87af21b4bc44c84ac5
yusuken1121 <yusuken1121@gmail.com>
2024-09-25 20:45:51 -0700

    ♻️ Replace with SSR (Server-Side-Rendering)

---
+++ frontend/app/(main)/properties/property/[propertyId]/loading.tsx
@@ -0,0 +1,12 @@
+import PropertyPageSkeleton from "@/components/molecules/propertyPage/SkeltonPropertyPage";
+import React from "react";
+
+const PropertyPageLoading = () => {
+  return (
+    <div>
+      <PropertyPageSkeleton />
+    </div>
+  );
+};
+
+export default PropertyPageLoading;
