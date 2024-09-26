0d25598544b96932c03347bca5d0cb4a02537a54
yusuken1121 <yusuken1121@gmail.com>
2024-09-25 21:36:42 -0700

    🐛 Fix the proper name of the router

--- backend/src/routes/propertyRouter.ts
+++ backend/src/routes/propertyRouter.ts
@@ -9,6 +9,6 @@ import {
 export const router = Router();
 
 router.get("/", getProperties);
-router.get("/page/:pageNumber", getPropertyPage);
+// router.get("/page/:pageNumber", getPropertyPage); // Function reserved for future use
 router.get("/:propertyId", getProperty);
 // router.get("/notion-schema", getSchema); // Function reserved for future use
