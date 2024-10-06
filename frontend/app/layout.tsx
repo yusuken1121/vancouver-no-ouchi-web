5327c8d0258d3634dfd42edff4d68f84e90b3ca4
yusuken1121 <yusuken1121@gmail.com>
2024-10-06 09:53:59 -0700

    ⚡️ Generate metadata based on device type (mobile or desktop)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,15 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { headers } from "next/headers";
+import { UAParser } from "ua-parser-js";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "VancouverNoOuchi",
-  description: "バンクーバーの生活に必要な情報を提供します",
-  icons: { icon: "/favicon.ico" },
-};
+export async function generateMetadata() {
+  const headersList = headers();
+  const ua = headersList.get("user-agent");
 
+  const device = new UAParser(ua || "").getDevice();
+
+  const isMobile = device.type === "mobile";
+
+  if (isMobile) {
+    return {
+      title: "バンクーバーのお家",
+      description:
+        "詐欺の心配一切なし。バンクーバーの安心安全お部屋探しサービス。語学留学やCoop留学を決めた方。渡航前に住む場所を決めたい方にも。シェアハウスやホームステイ、お問い合わせください！",
+      icons: { icon: "/favicon.ico" },
+    };
+  } else {
+    return {
+      title: "バンクーバーのお家",
+      description:
+        "詐欺の心配一切なし。日本からでもバンクーバーのお家が契約できるサービス。語学留学やCoop留学を決めたあなたに、家主と直接交渉をしたシェアハウスやホームステイなど、バンクーバーのお部屋をご紹介します。リモート内見も対応可能で、渡航前に住む場所を決めたい方にもピッタリ！まずはお気軽にご相談ください！",
+      icons: { icon: "/favicon.ico" },
+    };
+  }
+}
 export default function RootLayout({
   children,
 }: Readonly<{
