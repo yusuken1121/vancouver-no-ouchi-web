50ab461414bba233a15f34145d74c4f4044f4467
yusuken1121 <yusuken1121@gmail.com>
2024-11-03 20:35:20 -0800

    ⚡️ Modify backend controller to display properties even when count exceeds 100

--- backend/src/routes/propertyRouter.ts
+++ backend/src/routes/propertyRouter.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import {
-  getProperties,
+  getAllProperties, // over then 100
+  // getProperties, // less than 100
   getProperty,
-  getPropertyPage,
-  getSchema,
+  // getPropertyPage,
+  // getSchema,
 } from "../controllers/propertyController";
 
 export const router = Router();
 
-router.get("/", getProperties);
+// router.get("/", getProperties);
+router.get("/", getAllProperties);
 // router.get("/page/:pageNumber", getPropertyPage); // Function reserved for future use
 router.get("/:propertyId", getProperty);
 // router.get("/notion-schema", getSchema); // Function reserved for future use
